aca838f2cb2178ca61d58f8334b558632281efbd
Karl Anthony Salvador <31976047+salvadorK@users.noreply.github.com>
2020-04-05 17:55:10 -0400

    Chi branch (#3)
    * navbar
    
    * 95% done
    
    * almost done
    
    * sunday push
    
    Co-authored-by: Chi <chihungweb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import "./index.scss";
 import Mid from "./midpage/mid.jsx";
 import { Generic } from "rbx";
 import Steps from "./midpage/steps.jsx";
-import Nav from "./toppage/nav.jsx"; 
+import Nav from "./toppage/nav.jsx";  
 import Toppagebottom from "./toppagebottom/statement.jsx"; 
 import Carousel from "./toppage/carousel.jsx"; 
+import Edpage from "./educators/edpage.jsx"; 
+import Bottom from "./footer/footer.jsx"; 
+// import Yellow from "./toppage/yellowbottom.jsx"; 
+import Aboutus from "./toppage/aboutus.jsx"; 
+import Contactus from "./toppage/contactus.jsx"; 
+import Whymeditate from "./toppage/whymeditate.jsx";
+import Vidpage from "./midpage/vidpage.jsx";
 import Live from "./toppage/live.jsx";
 
 
@@ -20,9 +27,22 @@ class Index extends React.Component {
         <Nav/>
         <Carousel/>
         <Live />
+        {/* <Yellow/> */}
         <Toppagebottom />
         <Mid />
         <Steps />
+        <Vidpage/>
+        <Edpage/>
+        <Bottom/>
+        <Aboutus/>
+        <Contactus/>
+        <Whymeditate/>
+        
+    
+        
+        
+        
+        
     </Generic>
     
     );
--- src/toppagebottom/statement.jsx
+++ src/toppagebottom/statement.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Container, Column, Title} from 'rbx'
+import {Container, Column, Divider, Title} from 'rbx'
 import "./statement.scss"
 
 export default class Toppagebottom extends Component {
@@ -13,14 +13,13 @@ export default class Toppagebottom extends Component {
                       </Title>
                     </Column>
                 </Column.Group>
-                      
+                <Divider color="black">.</Divider>
                 <Column.Group>
                     <Column size="three-fifths"offset="one-fifth"> 
                         <Title className="font" subtitle>
-                        Meditate for Earth is a tool that allows anyone from around the world to find a <strong>common set time</strong> and join a collective of meditators who wish to meditate for Earth <strong>simultaneously</strong> in the objective to common set time <strong>amplify</strong> the effect. 
-                        By using the power of our collective energy, we can fuel the unified field with the feeling of love, peace and harmony and make a positive change for this world. 
-                        It‘s as simple as that and much needed in this present time. 
-                        By typing in your city in the search bar above,  you will find the <strong>two daily</strong> set times to join the collective and Meditate for Earth in the comfort of your home. 
+                        Meditate for Earth is a tool that allows anyone from around the world to find a common set time and join a collective of meditators who wish to meditate for Earth simultaneously. It‘s about finding a common time where eveyone can be connected.
+By using the power of our collective energy, we can amplify the effect and fuel the unified field with the feeling of love, peace and harmony and make a positive change for this world.  The more we are meditating at the same moment on earth, the greater the effect we’ll see.
+Find your daily local set time to join the collective and Meditate for Earth in the comfort of your home. 
                         </Title>
                     </Column>      
                 </Column.Group>
