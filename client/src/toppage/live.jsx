c64fdf88b510b81ab5a30b61122fc0177719ed2d
KAS <salvadorkab@gmail.com>
2020-04-17 13:08:11 -0400

    done

--- client/src/toppage/live.jsx
+++ client/src/toppage/live.jsx
@@ -12,22 +12,20 @@ export default class Live extends Component {
         this.state = {
             time: 0,
             counter: 0,
-            post: []
+            post:[]
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
         this.socket = socketIOClient('http://localhost:5000/');
-        this.socket.on('init',(data)=>{
+        this.socket.on('init',(data)=>
             this.setState({
                 post: data
             })
-        });
-        this.interval = setInterval(() => this.socket.on('init',(data)=>{
-            this.setState({
-                post: data
-            })
-        }), 1000);
+        );
+        this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
       }
     
     componentWillUnmount() {
@@ -41,7 +39,7 @@ export default class Live extends Component {
             location: moment.tz.guess(),
             date: date
         }
-        this.socket.emit('clicks', payload )
+        this.socket.emit('clicks', payload );
     }
     render() {
         let now = moment.tz("Europe/London");
@@ -58,23 +56,22 @@ export default class Live extends Component {
         let amresult = moment(now).isBetween(am1,pm1);
 
 
-        let start2 = new Date('2020-04-16T17:00:00.000Z').getTime()
-        let end2 = new Date('2020-04-16T21:00:00.000Z').getTime()
-
         let start1 = new Date(pm1).getTime()
         let end1 = new Date(pm2).getTime()
 
 
-        let result1 = this.state.post.length > 0 && this.state.post.filter(post => {var time = new Date(post.date).getTime();
+        let result1 = this.state.post && this.state.post.filter(post => {var time = new Date(post.date).getTime();
             return (start1 < time && time < end1);
            });
 
-        // let start2 = new Date(pm1).getTime()
-        // let end2 = new Date(pm2).getTime()
+        let start2 = new Date(pm1).getTime()
+        let end2 = new Date(pm2).getTime()
 
+        // let start2 = new Date('2020-04-16T17:00:00.000Z').getTime()
+        // let end2 = new Date('2020-04-17T20:00:00.000Z').getTime()
         
 
-        let result2 = this.state.post.length > 0 && this.state.post.filter(post => {var time = new Date(post.date).getTime();
+        let result2 = this.state.post && this.state.post.filter(post => {var time = new Date(post.date).getTime();
             return (start2 < time && time < end2);
            });
         
@@ -94,11 +91,11 @@ export default class Live extends Component {
                       </Title>
                       <Title className="counter" subtitle>
                      
-                           <Numeric> {result2.length} </Numeric>
+                           <Numeric> {amresult ? result1.length : result2.length} </Numeric>
                         </Title>
                         </Container>
                         
-                            <Button className="show" /*disabled={am || pm ? this.state.counter > 0 ? true : false : true }*/ onClick={()=>{this.handleSubmit()}}>
+                            <Button className="show" disabled={am || pm ? this.state.counter > 0 ? true : false : true }onClick={()=>{this.handleSubmit()}}>
                                 SHOW YOUR PRESENCE
                             </Button>
                     
