879e1eb2af36c81cf06e76def0db0d70c76c2cf7
Karl Anthony Salvador <31976047+salvadorK@users.noreply.github.com>
2020-04-22 21:39:02 -0400

    Chi (#8)
    * last
    
    * doneforreal
    
    * step3modified
    
    Co-authored-by: Chi <chihungweb@gmail.com>

--- client/src/midpage/steps.jsx
+++ client/src/midpage/steps.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Column, Title, Generic, Heading} from 'rbx'
+import { Column, Title, Generic} from 'rbx'
 import "./steps.scss"
 
 export default class Steps extends Component {
@@ -85,11 +85,7 @@ export default class Steps extends Component {
                                                 
                             <Title className="stepscontent" subtitle>
             
-                                You can do you own personal meditation by feeling love and harmony for our Earth
-                                <Heading><strong>or</strong></Heading>
-                                Listen to guided meditations that focus intention on our mother Earth
-                                <Heading><strong>or</strong></Heading>
-                                Join influencers on live Earth Meditations 
+                                Sit in a comfortable and relaxing environment. Imagine earth shrinking to the size where she can fit in the palm of your hand. Gently place her in the center of your heart and feel your loving connection. Tell Her and let Her truly feel: My heart is your heart, we are One. Your heart is my heart, we are One. Repeat this exercise, she’s a living being and will feel this love. Conduct the rest of the meditation with your heart.
    
                             </Title>                  
                         </Column>
